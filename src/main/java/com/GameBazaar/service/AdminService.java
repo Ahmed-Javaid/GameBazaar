cfe17e3f77b7d0615e24625976865ba6fc66366b
Ahmed <ahmadjavaid890@gmail.com>
2024-06-06 00:27:28 +0500

    2 Services added

---
+++ src/main/java/com/GameBazaar/service/AdminService.java
@@ -0,0 +1,13 @@
+package com.GameBazaar.service;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.GameBazaar.Dao.AdminDao;
+import com.GameBazaar.model.Admin;
+
+@Service
+public class AdminService {
+
+
+}
